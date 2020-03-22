9f20368f4f973727fb99f5ab6a4855b51c01bc42
excircle <unixdisiple.us@gmail.com>
2020-03-21 19:14:01 -0700

    adding removeVowels.go

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # General Golang Code
 
 This repository is dedicated to small coding challenges and examples for my own personal reference
+
+<b>challenges</b> - Coding challenges in Go
---
+++ challenges/removeVowels.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+import "strings"
+
+func removeVowels(s string) string {
+	l := []string{"a", "e", "i", "o", "u"}
+	for _, v := range l {
+		s = strings.Replace(s, string(v), "", -1)
+	}
+	return s
+}
+
+func main() {
+	testString := "This is a phrase"
+	result := removeVowels(testString)
+	fmt.Printf("%v\n%v", testString, result)
+}
